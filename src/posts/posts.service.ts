37dacf8a8e29341d1fcd4c75d506f5ee273c5013
Chris Tam <tamkachun2908@gmail.com>
2021-08-22 21:27:18 +0800

    feat: post udpate with authorization

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -18,7 +18,7 @@ export class PostsService {
     private readonly authService: AuthService
   ) { }
 
-  create(user: jwtUser, createPostDto: CreatePostDto) {
+  async create(user: jwtUser, createPostDto: CreatePostDto) {
     // This action adds a new post
     let createPost = {
       ...createPostDto,
@@ -28,22 +28,32 @@ export class PostsService {
     return createdPost.save();
   }
 
-  findAll() {
+  async findAll() {
     // This action returns all posts
     return this.postModel.find().exec()
   }
 
-  findOne(id: string) {
+  async findOne(id: string) {
     // This action returns a ${id} post
     return this.postModel.findById(id)
   }
 
-  update(id: string, user: jwtUser, updatePostDto: UpdatePostDto) {
+  async update(id: string, user: jwtUser, updatePostDto: UpdatePostDto) {
     // This action updates a ${id} post
+    let updateTarget = await this.findOne(id)
+    if(!updateTarget){
+      return false
+    }
+
+    let canModify = await this.authService.valideUserCanModify(user.id, updateTarget.id)
+    if(!canModify){
+      return false
+    }
+
     return this.postModel.findByIdAndUpdate(id, updatePostDto, { useFindAndModify: false })
   }
 
-  remove(id: string) {
+  async remove(id: string) {
     // This action removes a ${id} post
     return this.postModel.findByIdAndRemove(id, { useFindAndModify: false })
   }
