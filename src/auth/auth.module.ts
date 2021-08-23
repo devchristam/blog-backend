4af471965749f67b5d516dcab5def43a3aec2a96
Chris Tam <tamkachun2908@gmail.com>
2021-08-23 22:33:29 +0800

    feat: set jwt time at secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { jwtConstants } from '../../secret/constants';
 @Module({
   imports: [UsersModule, PassportModule, JwtModule.register({
     secret: jwtConstants.secret,
-    signOptions: { expiresIn: '60s' }
+    signOptions: { expiresIn: `${jwtConstants.jwtTime}s` }
   })],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, LocalStrategy],
